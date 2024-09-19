4e041dd11e0b69734116dd9776583838e2bd0e4f
Oscar Sanchez <oscar.s.m0711@gmail.com>
2024-09-19 13:35:48 -0500

    chore: init project blank

---
+++ src/test/java/com/castor/administracion/AdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castor.administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
